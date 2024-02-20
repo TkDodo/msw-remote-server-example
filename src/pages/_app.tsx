7bd6409cbb8ee062a2139e7d268ae02f7edda7a7
Dominik Dorfmeister <office@dorfmeister.cc>
2024-02-20 13:56:07 +0100

    try remote intercept in playwright

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,12 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { MockProvider } from "~/MockProvider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <MockProvider>
+    <>
       <Component {...pageProps} />
-    </MockProvider>
+    </>
   );
 };
 
--- tests/post.spec.ts
+++ tests/post.spec.ts
@@ -1,8 +1,26 @@
 import { test, expect } from "@playwright/test";
 
+import { setupRemoteServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+
+const remote = setupRemoteServer();
+
+test.beforeAll(async () => await remote.listen({ port: 3000 }));
+test.afterEach(() => remote.resetHandlers());
+test.afterAll(async () => await remote.close());
+
 test("renders post", async ({ page }) => {
+  remote.use(
+    http.get("*", () => {
+      return HttpResponse.json({
+        id: 2,
+        title: "Hello MSW",
+      });
+    }),
+  );
+
   await page.goto("http://127.0.0.1:3000/");
 
   await expect(page.getByText("Loading tRPC query...")).toBeVisible();
-  await expect(page.getByText("sunt aut facere")).toBeVisible();
+  await expect(page.getByText("Hello MSW")).toBeVisible();
 });
